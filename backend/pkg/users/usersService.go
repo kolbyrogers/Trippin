cd5ce15433be097f4f5bd2e9f2d4489cb3000f0f
kamden-rasmussen <kamdenr@gmail.com>
2022-11-11 13:17:33 -0700

    able to pull data down

--- backend/pkg/users/usersService.go
+++ backend/pkg/users/usersService.go
@@ -1,9 +1,11 @@
 package users
 
-// package firebase
+import (
+	"context"
+	"fmt"
 
-
-
+	firestore "cloud.google.com/go/firestore"
+)
 
 type Repository interface {
 	GetUser(ID string) (User, error)
@@ -18,6 +20,8 @@ type Service interface {
 type service struct {
 	// r Repository
 	user User
+	DB   firestore.Client
+	ctx context.Context
 }
 
 type User struct {
@@ -28,11 +32,32 @@ type User struct {
 	
 }
 
-func NewService() *service {
-	return &service{User{}}
+func NewService(DB firestore.Client, ctx context.Context) *service {
+	return &service{User{}, DB, ctx}
 }
 
 func (s *service) GetUser(ID string) (User, error) {
+	resp, err := s.DB.Collection("users").Doc(ID).Get(s.ctx)
+	if err != nil {
+		fmt.Println(err)
+		return User{}, err
+	}
+	
+	data := resp.Data()
+
+	fmt.Println(data)
+	// fmt.Println(resp)
+	// body, err := ioutil.ReadAll(resp)
+
+	// // unmarshal user into User struct
+	// returnuser, err := Unmarshal(body, &User{})
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+
+	// fmt.Println(user)
+	
 	return User{}, nil
 }
 
