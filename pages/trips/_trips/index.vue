645898eb32ac0ff3692d7344a421fcf5c3b9444f
Reidemption <75095757+Reidemption@users.noreply.github.com>
2022-11-11 15:10:54 -0700

    fixed

--- pages/trips/_trips/index.vue
+++ pages/trips/_trips/index.vue
@@ -1,53 +1,53 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="2">
-                <v-btn color="primary" class="white--text" @click="back">
-                    Back
-                </v-btn>
-            </v-col>
-            <v-col offset="6" cols="2">
-                <v-btn color="primary" class="white--text" @click="directToShare">
-                    Share
-                </v-btn>
-            </v-col>
-            <v-col cols="2">
-                <v-btn color="primary" class="white--text" @click="directToNewEvent">
-                    New Event
-                </v-btn>
-            </v-col>
-        </v-row>
-        <h1>Trip Location</h1>
-        <v-row v-for="event in events" :key="event.id">
-            <Event :event="event"></Event>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="2">
+        <v-btn color="primary" class="white--text" @click="back"> Back </v-btn>
+      </v-col>
+      <v-col offset="6" cols="2">
+        <v-btn color="primary" class="white--text" @click="directToShare">
+          Share
+        </v-btn>
+      </v-col>
+      <v-col cols="2">
+        <v-btn color="primary" class="white--text" @click="directToNewEvent">
+          New Event
+        </v-btn>
+      </v-col>
+    </v-row>
+    <h1>Trip Location</h1>
+    <v-row v-for="event in events" :key="event.id">
+      <Event :event="event"></Event>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-            events: [
-                {
-                    id: 12,
-                    name: "Snorkeling",
-                    location: "Coral Reef",
-                    image: "/images/snorkel.jpg",
-                }
-            ]
-        }
-    },
-    methods:
-    {
-        directToNewEvent() {
+  async asyncData({ params }) {
+    const id = params.id
+    return { id }
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 12,
+          name: 'Snorkeling',
+          location: 'Coral Reef',
+          image: '/images/snorkel.jpg',
         },
-        directToShare() {
-        },
-        back() {
-            this.$router.go(-1)
-        }
+      ],
     }
+  },
+  methods: {
+    directToNewEvent() {},
+    directToShare() {
+      this.$router.push('/trips/' + this.id + '/share')
+    },
+    back() {
+      this.$router.go(-1)
+    },
+  },
 }
 </script>
