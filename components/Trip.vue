bae0866509898b796bacde98287b3edc4a802045
Kolby Rogers <kolbyrogers1@gmail.com>
2022-11-11 13:49:31 -0700

    add Trip component

---
+++ components/Trip.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card width="100%" style="border: 1px solid #424242;" class="mb-5 text-left">
+        <v-img :src="this.image" height="300px" gradient="to top left, rgba(100,115,201,.33), rgba(25,32,72,.7)">
+            <h2 class="pa-4 white--text">{{ this.location }}</h2>
+        </v-img>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: ['location', 'date', 'image'],
+    data() {
+        return {
+        }
+    }
+}
+</script>
