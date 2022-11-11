020de1341ab8d46a0c94c99bfdafc841192d02f6
kamden-rasmussen <kamdenr@gmail.com>
2022-11-11 14:18:50 -0700

    able to add a user

--- backend/pkg/handlers/handleUsers.go
+++ backend/pkg/handlers/handleUsers.go
@@ -3,12 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/kolbyrogers/Trippin/backend/pkg/users"
 )
-
 
 func getUserHandler(usersService users.Service) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +30,35 @@ func getUserHandler(usersService users.Service) func(w http.ResponseWriter, r *h
 
 func addUserHandler(usersService users.Service) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("Hit addUserHandler")
+		var newUser users.User
+		
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode("Error reading request body " + err.Error())
+			return
+		}
+		fmt.Println(string(bodyBytes[:]))
+
+		// convert bodyBytes to a NewUser struct
+		err = json.Unmarshal(bodyBytes, &newUser)
+
+		// &userStruct.Email = string(bodyBytes["email"])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode("Error unmarshalling request body " + err.Error())
+			return
+		}
+		fmt.Println(newUser)
+
+		_, err = usersService.AddUser(newUser)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
 	}
 }
